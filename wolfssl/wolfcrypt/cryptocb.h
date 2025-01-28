c93a533ad42b98f821e5081fb6c24b3b14a0a2d9
Tobias Frauenschläger <tobias.frauenschlaeger@oth-regensburg.de>
2025-01-28 08:49:36 +0100

    Improved PKCS#11 certificate support
    * Support for storing decoded certificates in a PKCS#11 token (entity,
      intermediate and root certificates)
    * Support for loading an intermediate certifcate from a token for a TLS
      context

--- wolfssl/wolfcrypt/cryptocb.h
+++ wolfssl/wolfcrypt/cryptocb.h
@@ -541,6 +541,7 @@ typedef struct wc_CryptoInfo {
         word32 idLen;
         const char *label;
         word32 labelLen;
+        int certType;
         byte **certDataOut;
         word32 *certSz;
         int *certFormatOut;
@@ -888,7 +889,7 @@ WOLFSSL_LOCAL int wc_CryptoCb_SheExportKey(wc_SHE* she,
 #ifndef NO_CERTS
 WOLFSSL_LOCAL int wc_CryptoCb_GetCert(int devId, const char *label,
     word32 labelLen, const byte *id, word32 idLen, byte** out,
-    word32* outSz, int *format, void *heap);
+    word32* outSz, int *format, int certType, void *heap);
 #endif
 
 #ifdef WOLF_CRYPTO_CB_COPY
