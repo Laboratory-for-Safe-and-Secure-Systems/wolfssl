bb183eccb998caf10f244162efa716840a364ca0
Tobias Frauenschläger <tobias.frauenschlaeger@oth-regensburg.de>
2025-02-07 16:11:19 +0100

    Fix memory leak

--- src/internal.c
+++ src/internal.c
@@ -9032,6 +9032,10 @@ void FreeHandshakeResources(WOLFSSL* ssl)
         FreeKey(ssl, DYNAMIC_TYPE_FALCON, (void**)&ssl->peerFalconKey);
         ssl->peerFalconKeyPresent = 0;
 #endif /* HAVE_FALCON */
+#if defined(HAVE_DILITHIUM)
+        FreeKey(ssl, DYNAMIC_TYPE_DILITHIUM, (void**)&ssl->peerDilithiumKey);
+        ssl->peerDilithiumKeyPresent = 0;
+#endif /* HAVE_DILITHIUM */
     }
 
 #ifdef HAVE_ECC
