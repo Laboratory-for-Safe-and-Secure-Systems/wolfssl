3438c9dfec9e814dfd3c72dd119c2d3c44aa14a1
Tobias Frauenschläger <tobias.frauenschlaeger@oth-regensburg.de>
2025-02-27 13:36:48 +0100

    Fix Kyber SMALL_MEM warning

--- wolfcrypt/src/wc_mlkem.c
+++ wolfcrypt/src/wc_mlkem.c
@@ -672,8 +672,8 @@ static int mlkemkey_encapsulate(MlKemKey* key, const byte* m, byte* r, byte* c)
     sword16 y[3 * MLKEM_MAX_K * MLKEM_N];
 #endif
 #endif
-    sword16* u;
-    sword16* v;
+    sword16* u = NULL;
+    sword16* v = NULL;
 
     /* Establish parameters based on key type. */
     switch (key->type) {
