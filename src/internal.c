20b36e3815384e793ab7bf850faa767c270e4eab
Tobias Frauenschläger <tobias.frauenschlaeger@oth-regensburg.de>
2025-03-27 12:24:45 +0100

    Fixes for PSK Handshake without Diffie-Hellman
    * Make sure no KeyShare is sent from the client when DH is disabled in PSK mode
    * Fix server error in Suite matching in case no KeyShare is present

--- src/internal.c
+++ src/internal.c
@@ -37769,6 +37769,14 @@ static int AddPSKtoPreMasterSecret(WOLFSSL* ssl)
         }
 #endif
 
+#if !defined(NO_PSK)
+        if(havePSK && ssl->options.noPskDheKe) {
+            /* In this case, no keyshare extension is sent from the client and
+             * we can exit the verification successfully. */
+            return 1;
+        }
+#endif
+
 #ifdef WOLFSSL_TLS13
         if (IsAtLeastTLSv1_3(ssl->version) &&
                                       ssl->options.side == WOLFSSL_SERVER_END) {
@@ -37992,7 +38000,11 @@ static int AddPSKtoPreMasterSecret(WOLFSSL* ssl)
 
 #if defined(WOLFSSL_TLS13) && defined(HAVE_SUPPORTED_CURVES)
         if (IsAtLeastTLSv1_3(ssl->version) &&
-                                      ssl->options.side == WOLFSSL_SERVER_END) {
+                                         ssl->options.side == WOLFSSL_SERVER_END
+    #if !defined(NO_PSK)
+            && !ssl->options.noPskDheKe
+    #endif
+        ) {
             ret = TLSX_KeyShare_Setup(ssl, cs.clientKSE);
             if (ret != 0)
                 return ret;
