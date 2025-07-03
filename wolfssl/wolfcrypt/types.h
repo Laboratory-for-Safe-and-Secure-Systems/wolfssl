cc4cfafa062205286ba52143a52be65abc2c9871
Tobias Frauenschläger <tobias.frauenschlaeger@oth-regensburg.de>
2025-07-03 16:30:42 +0200

    Add support for PKCS#11 CA certificates
    The new method `wolfSSL_CTX_load_verify_dev()` can be used to load all
    available CA certificates from a CrytoCb device and add them to the
    CertManager trust store. Certificates which fail to parse are simply
    ignored.
    
    Furthermore, support is added to the PKCS#11 interface to search for all
    trusted CA certificates on a token. The certificates are read into RAM
    and then added to the trust store.
    
    Signed-off-by: Tobias Frauenschläger <tobias.frauenschlaeger@oth-regensburg.de>

--- wolfssl/wolfcrypt/types.h
+++ wolfssl/wolfcrypt/types.h
@@ -1220,8 +1220,9 @@ enum wc_AlgoType {
     WC_ALGO_TYPE_HMAC = 6,
     WC_ALGO_TYPE_CMAC = 7,
     WC_ALGO_TYPE_CERT = 8,
+    WC_ALGO_TYPE_CA_CERTS = 9,
 
-    WC_ALGO_TYPE_MAX = WC_ALGO_TYPE_CERT
+    WC_ALGO_TYPE_MAX = WC_ALGO_TYPE_CA_CERTS
 };
 
 /* hash types */
